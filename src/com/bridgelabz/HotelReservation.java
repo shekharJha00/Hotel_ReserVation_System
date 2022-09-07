32ed1d25f2f6fabb52b248c02fe35ca7fc47a9c3
shekhar <shekharjha7417870578@gmail.com>
2022-09-07 05:52:12 +0530

    Welcome to Hotel Reservation Program

---
+++ src/com/bridgelabz/HotelReservation.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+
+        HotelReservation hotel = new HotelReservation();
+
+        System.out.println("--Welcome to Hotel Reservation Program--");
+
+
+
+    }
+
+}
+
